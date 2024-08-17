5918f9414b34a958fc12385bcd4f4c85a6d3b448
DonFredi <dondadi.fn@gmail.com>
2024-08-17 21:32:29 +0300

    ,,,

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://donfredi.github.io/portfolio/',
+  base: '/portfolio/',
   build: {
     outDir: 'build', // Specify the output directory as 'build'
   },
